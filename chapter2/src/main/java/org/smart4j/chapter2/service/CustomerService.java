2da7edfb320ac5134d44a67e79349801d1541586
wangzhen <376086383@qq.com>
2018-05-24 10:55:08 +0800

    完善服务层(写死在Service中的数据库连接信息)-优化前

--- chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -1,7 +1,11 @@
 package org.smart4j.chapter2.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.smart4j.chapter2.model.Customer;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,11 +15,46 @@ import java.util.Map;
  */
 public class CustomerService {
 
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/demo";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "123";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
     /**
      * 获取客户列表
      */
     public List<Customer> getCustomerList() {
-        // TODO
+        Connection conn = null;
+        try{
+            List<Customer> customerList = new ArrayList<Customer>();
+            String sql = "select * from customer";
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                Customer customer = new Customer();
+                customer.setId(rs.getLong("id"));
+                customer.setName(rs.getString("name"));
+                customer.setContact(rs.getString("contact"));
+                customer.setTelephone(rs.getString("telephone"));
+                customer.setEmail(rs.getString("email"));
+                customer.setRemark(rs.getString("remark"));
+                customerList.add(customer);
+            }
+            return customerList;
+        }catch(SQLException e){
+            LOGGER.error("execute sql failure", e);
+        }finally {
+            if(conn != null){
+                try{
+                    conn.close();
+                }catch(SQLException e){
+                    LOGGER.error("close connection failure", e);
+                }
+            }
+        }
         return null;
     }
 
